cc73cd6d7d1e4c7ef8be4f8ce18a705570763935
JP-GS <juanpgordillo89@gmail.com>
2022-06-14 19:03:52 -0300

    Separacion de los repositorios de frontend y backend en github

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { IniciarSesionComponent } from './components/iniciar-sesion/iniciar-sesion.component';
+
+const routes: Routes = [
+  {path:'', component: HomeComponent},
+  {path:'login', component: IniciarSesionComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
