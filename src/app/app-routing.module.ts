c65fc3888af8e1882df4369ac40984444b15d25e
JP-GS <juanpgordillo89@gmail.com>
2022-06-20 23:37:43 -0300

    Correccion en estilos de experiencias y educacion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,9 +4,9 @@ import { HomeComponent } from './components/home/home.component';
 import { IniciarSesionComponent } from './components/iniciar-sesion/iniciar-sesion.component';
 
 const routes: Routes = [
-  {path:'home', component: HomeComponent},
-  {path:'login', component: IniciarSesionComponent},
-  {path:'', redirectTo: 'login', pathMatch:'full'}
+  {path:'', component: HomeComponent},
+  {path:'login', component: IniciarSesionComponent}
+  // {path:'', redirectTo: 'login', pathMatch:'full'}
 ];
 
 @NgModule({
